0a2e95c4d9c43c92b6ac2c81abc1041285744d26
Hector Alfonso <haalfonsoc@ufpso.edu.co>
2024-11-15 17:31:14 -0500

    Vue

---
+++ src/components/vue/Notification.vue
@@ -0,0 +1,48 @@
+<script setup>
+    import {ref} from 'vue'
+
+    let qty = ref(1)
+
+    function add(){
+        qty.value++   
+        console.log(qty)
+    }
+    function sustrade(){
+        if (qty.value>0){
+            qty.value--
+        }else{
+            console.log('Pero tilin?')
+        }
+        
+        console.log(qty)
+    }
+</script>
+<template>
+    <div class="bg-purple-300 rounded max-w-80 mx-auto p-6">
+        <div 
+            v-if="qty > 0"
+            class="bg-red-500 rounded-full text-white size-8 flex justify-center items-center font-bold"
+            :class="{'bg-orange-500': qty > 101}"
+            
+        >
+            {{ qty }}
+        </div>
+        <div v-else>
+            Nadie te visita :(
+        </div>
+        <div class="flex">
+            <div
+            @click="add"
+            class="bg-blue-400 text-white rounded size-6 flex justify-center items-center mr-4 hover:cursor-pointer">
+            +
+        </div>
+        <div
+            @click="sustrade"
+            class="bg-blue-400 text-white rounded size-6 flex justify-center items-center hover:cursor-pointer">
+            -
+        </div>
+        </div>
+        
+    </div>
+    
+</template>
